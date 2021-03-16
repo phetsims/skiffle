5e418418067c4d862335e8b97ebcc940924a340b
jbphet <john.blanco@colorado.edu>
2021-03-16 14:24:16 -0600

    initial gruntfile, HTML template, and built sound-board page

---
+++ js/Gruntfile.js
@@ -0,0 +1,104 @@
+// Copyright 2021, University of Colorado Boulder
+
+/**
+ * sound-board-specific grunt configuration
+ *
+ * @author John Blanco (PhET Interactive Simulations)
+ */
+
+'use strict';
+
+// modules
+const grunt = require( 'grunt' ); // eslint-disable-line
+const winston = require( 'winston' );
+
+// constants
+const ACTIVE_REPOS_FILE = '../perennial/data/active-repos';
+
+module.exports = grunt => {
+
+  grunt.registerTask(
+    'build',
+    'Build the sound board HTML file',
+    () => {
+
+      // Read in the list of active repositories, splitting the lines into separate strings.
+      let activeRepos = grunt.file.read( ACTIVE_REPOS_FILE ).split( '\n' );
+
+      // On at least some systems, such as Windows, splitting the lines up this way results in there being a carriage
+      // return character at the end of the string representing the repo name.  Remove it.
+      activeRepos = activeRepos.map( activeRepo => activeRepo.replace( '\r', '' ) );
+
+      // Remove any blank lines from the list of active repos.
+      activeRepos = activeRepos.filter( activeRepo => activeRepo.length > 0 );
+
+      // Make a list of all repos with sound files.
+      const reposWithSoundFiles = activeRepos.filter( activeRepo => {
+        const pathToCheck = '../' + activeRepo + '/sounds';
+        return grunt.file.exists( pathToCheck );
+      } );
+
+      // The string that will ultimately contain the HTML that will list the sound files and allow a user to play them.
+      let soundControlHtml = '';
+
+      // For each repo with sound, create collapsible button with content beneath it to play each of the sounds by
+      // pressing a button.
+      reposWithSoundFiles.forEach( repoWithSoundFiles => {
+
+        // Add the HTML for the collapsible button for this repo.
+        soundControlHtml += `  <button type="button" class="collapsible">${repoWithSoundFiles}</button>\n`;
+        soundControlHtml += '  <div class="content">\n';
+
+        // Get a list of the sounds for this repo.
+        const pathToSoundsDirectory = '../' + repoWithSoundFiles + '/sounds/';
+        const patterns = [ pathToSoundsDirectory + '*.mp3', pathToSoundsDirectory + '*.wav' ];
+        const soundFileNames = grunt.file.expand( { filter: 'isFile' }, patterns );
+
+        // Create buttons for the sounds and group them together.
+        const numberOfSoundsPerButtonGroup = 4;
+        for ( let i = 0; i < soundFileNames.length; i += numberOfSoundsPerButtonGroup ){
+
+          // Output HTML for the button group.
+          soundControlHtml += '    <div class="btn-group">\n';
+
+          // Extract the subset of sounds that will be controlled by this button group.
+          const soundsFilesForButtonGroup = soundFileNames.slice( i, i + numberOfSoundsPerButtonGroup );
+
+          // Create a button for each of the sounds.
+          soundsFilesForButtonGroup.forEach( soundFile => {
+            const soundFileNameOnly = soundFile.substring( soundFile.lastIndexOf( '/' ) + 1 );
+
+            // If the name of the sound file is too long, create a shortened version with an ellipsis.
+            const maxButtonLabelLength = 20;
+            let buttonLabel;
+            if ( soundFileNameOnly.length > maxButtonLabelLength ){
+              buttonLabel = soundFileNameOnly.substring( 0, maxButtonLabelLength - 3 ) + '...';
+            }
+            else{
+              buttonLabel = soundFileNameOnly;
+            }
+            soundControlHtml += `      <button title="${soundFileNameOnly}" onClick="playSound( '../${soundFile}' )">${buttonLabel}</button>\n`;
+          } );
+
+          // Close the button group div
+          soundControlHtml += '    </div>\n';
+        }
+
+        // Close the tags for this section.
+        soundControlHtml += '  </div>\n';
+      } );
+
+      // Read in the HTML file template.
+      const soundBoardTemplateHtml = grunt.file.read( './html/sound-board-template.html' );
+      const soundBoardHtml = soundBoardTemplateHtml.replace( '{{SOUND_CONTROL_CONTENT}}', soundControlHtml );
+
+      // Write the HTML file with the sound control content filled in.
+      grunt.file.write( './html/sound-board.html', soundBoardHtml );
+
+      winston.info( 'Done.' );
+    }
+  );
+
+  // register default task
+  grunt.registerTask( 'default', [ 'build' ] );
+}
