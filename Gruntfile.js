2ef07f09a3ca5eb863799785da81b24ce8ac8149
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-11-15 12:52:20 -0700

    move commonjs modules to their own directory, https://github.com/phetsims/perennial/issues/410

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,7 @@
 // Copyright 2021-2024, University of Colorado Boulder
 
 const Gruntfile = require( '../chipper/Gruntfile' );
-const registerTasks = require( '../perennial-alias/js/grunt/util/registerTasks' );
+const registerTasks = require( '../perennial-alias/js/grunt/commonjs/registerTasks' );
 
 /**
  * Dot grunt tasks
